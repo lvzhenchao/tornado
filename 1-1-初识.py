cf8b609dda2a633bac4c678371ff52bb63114289
lvzhenchao <1370164516@qq.com>
2023-04-26 16:48:56 +0800

    Create 1-1-初识.py

---
+++ 1-1-初识.py
@@ -0,0 +1,29 @@
+import tornado.web # 基础web框架模块
+import tornado.ioloop # 核心IO循环模块， 封装了Linux的epoll和BSD的kqueue,是框架高效的基础
+
+# 业务处理类，习惯加个handler
+class IndexHandler(tornado.web.RequestHandler):
+
+    # 处理get请求的，不能处理post请求
+    def get(self, *args, **kwargs):
+        # 对应http请求的方法
+        # 给浏览器响应信息
+        self.write("lzc is a good man")
+
+
+if __name__ == "__main__":
+
+    # 用这个类：tornado.web.Application，实例化一个应用对象
+    # Application：是tornado web 框架的核心应用类，是与服务对应的接口
+    # 里面保存了路由映射表
+    app = tornado.web.Application([
+        (r"/", IndexHandler)
+    ])
+
+    # 有一个listen方法来创建一个http服务器的实例，并绑定了监听端口
+    # 注意：此时服务器并没有开启监听
+    app.listen(8888)
+
+    # IOLoop.current()：返回当前线程的IOLoop实例对象
+    # IOLoop.start()：启动IOLoop实例对象的I/O循环
+    tornado.ioloop.IOLoop.current().start()
