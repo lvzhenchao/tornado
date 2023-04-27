700f875e241d6528f2ddfc861daf49a311d67643
lvzhenchao <1370164516@qq.com>
2023-04-27 17:32:25 +0800

    Update 1-3-options模块.py

--- 1-3-options模块.py
+++ 1-3-options模块.py
@@ -1,10 +1,13 @@
 import tornado.web # 基础web框架模块
 import tornado.ioloop # 核心IO循环模块， 封装了Linux的epoll和BSD的kqueue,是框架高效的基础
 import tornado.httpserver
-import tornado.options
+from tornado import options
+
+# 定义两个参数
+tornado.options.define("port", default = 8899, type = int)
+tornado.options.define("list", default = [], type = str, multiple=True)
 
 
-tornado.options.define("port", 8899, int)
 
 # 业务处理类，习惯加个handler
 class IndexHandler(tornado.web.RequestHandler):
@@ -18,24 +21,21 @@ class IndexHandler(tornado.web.RequestHandler):
 
 if __name__ == "__main__":
 
-    # 用这个类：tornado.web.Application，实例化一个应用对象
-    # Application：是tornado web 框架的核心应用类，是与服务对应的接口
-    # 里面保存了路由映射表
+    # 转换命令行参数，并保存到tornado.options.options
+    tornado.options.parse_command_line()
+    print("list = ", tornado.options.options.list)
+    print("port = ", tornado.options.options.port)
+
     app = tornado.web.Application([
         (r"/", IndexHandler)
     ])
 
-    # 有一个listen方法来创建一个http服务器的实例，并绑定了监听端口
-    # 注意：此时服务器并没有开启监听
-    # 1、这是一种创建HTTPServer，并且同时绑定一个端口
-    # app.listen(8888)
 
     # 2、手动创建一个http服务器对象，这种更直观看到创建服务器了
     httpServer = tornado.httpserver.HTTPServer(app)
     # 单独给这个服务器绑定端口
-    # httpServer.listen(8888)
-    httpServer.bind(port)
-    httpServer.start(5)
+    httpServer.bind(tornado.options.options.port)
+    httpServer.start(1)
 
     # IOLoop.current()：返回当前线程的IOLoop实例对象
     # IOLoop.start()：启动IOLoop实例对象的I/O循环，同时开启了监听
@@ -43,3 +43,4 @@ if __name__ == "__main__":
 
 # options 基础方法和属性
 ## define()方法：定义变量
+## parse_command_line()方法：解析命令行的参数，并保存到options
