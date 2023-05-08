a9a6398570aef7056de5bf220961f11353d845d6
lvzhenchao <1370164516@qq.com>
2023-05-08 15:34:53 +0800

    基础

---
+++ 2-0请求与响应/config.py
@@ -0,0 +1,7 @@
+
+# 参数，一般是字典
+
+options = {
+    "port":8081,
+    "list":["good","nice"]
+}
---
+++ 2-0请求与响应/server.py
@@ -0,0 +1,32 @@
+import tornado.web # 基础web框架模块
+import tornado.ioloop # 核心IO循环模块， 封装了Linux的epoll和BSD的kqueue,是框架高效的基础
+import tornado.httpserver
+from config import options
+
+# 业务处理类，习惯加个handler
+class IndexHandler(tornado.web.RequestHandler):
+
+    # 处理get请求的，不能处理post请求
+    def get(self, *args, **kwargs):
+        # 对应http请求的方法
+        # 给浏览器响应信息
+        self.write("lzc is a good man")
+
+
+if __name__ == "__main__":
+
+    print("list = ", options['list'])
+
+    app = tornado.web.Application([
+        (r"/", IndexHandler)
+    ])
+
+    # 手动创建一个http服务器对象，这种更直观看到创建服务器了
+    httpServer = tornado.httpserver.HTTPServer(app)
+    # 单独给这个服务器绑定端口
+    httpServer.bind(options['port'])
+    httpServer.start(1)
+
+    # IOLoop.current()：返回当前线程的IOLoop实例对象
+    # IOLoop.start()：启动IOLoop实例对象的I/O循环，同时开启了监听
+    tornado.ioloop.IOLoop.current().start()
