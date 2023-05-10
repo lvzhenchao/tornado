9d6c9e42c7fe0af22c0bbaebfd82b4b20f36999c
锐源 <1370164516@qq.com>
2023-05-10 23:33:51 +0800

    g

--- 2-0请求与响应/server.py
+++ 2-0请求与响应/server.py
@@ -2,14 +2,23 @@ import tornado.web # 基础web框架模块
 import tornado.ioloop # 核心IO循环模块， 封装了Linux的epoll和BSD的kqueue,是框架高效的基础
 import tornado.httpserver
 from config import options
-
-from application import Application
-
-
+import views
 
 if __name__ == "__main__":
 
-    app = Application()
+    app = tornado.web.Application([
+    #     # from views import index
+    #     # (r"/", index.IndexHandler)
+    #
+    #     # import views.index
+    #     # (r"/", views.index.IndexHandler)
+    #
+    #     # from views.index import IndexHandler
+    #     # (r"/", IndexHandler)
+    #
+    #     # import view
+        (r"/", views.index.IndexHandler)
+    ])
 
     # 手动创建一个http服务器对象，这种更直观看到创建服务器了
     httpServer = tornado.httpserver.HTTPServer(app)
