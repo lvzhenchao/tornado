8cf027db34cb1fe39a4099da95dd69ec4e2f8d36
锐源 <1370164516@qq.com>
2023-05-11 20:55:30 +0800

    Update application.py

--- 2-0请求与响应/application.py
+++ 2-0请求与响应/application.py
@@ -5,17 +5,6 @@ from views import index
 class Application(tornado.web.Application):
     def __init__(self):
         handlers = [
-            # app = tornado.web.Application([
-            # from views import index
-            # (r"/", index.IndexHandler)
-
-            # import views.index
-            # (r"/", views.index.IndexHandler)
-
-            # from views.index import IndexHandler
-            # (r"/", IndexHandler)
-
-            # import view
             (r"/", index.IndexHandler),
             (r"/home", index.HomeHandler)
         ]
