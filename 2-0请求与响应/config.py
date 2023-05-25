2874e63ea0f514f91952d2616e82a2b802c96795
锐源 <1370164516@qq.com>
2023-05-25 21:02:13 +0800

    Update config.py

--- 2-0请求与响应/config.py
+++ 2-0请求与响应/config.py
@@ -1,11 +1,16 @@
+import os.path
+
+# //生成当前文件的路径
+BASE_DIRS = os.path.dirname(__file__)
 
 # 参数，一般是字典
-
 options = {
     "port":8081,
 }
 
 # 配置
 settings = {
-    "debug" : True
+    "debug" : True,
+    "static_path": os.path.join(BASE_DIRS, "static"),
+    "template_path": os.path.join(BASE_DIRS, "temlpates"),
 }
