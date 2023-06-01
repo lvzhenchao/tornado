c4771af311cd6f62f8b49541df81a727f68233bc
锐源 <1370164516@qq.com>
2023-06-01 22:03:09 +0800

    Update config.py

--- 2-0请求与响应/config.py
+++ 2-0请求与响应/config.py
@@ -12,8 +12,8 @@ options = {
 settings = {
     "static_path": os.path.join(BASE_DIRS, "static"),
     "template_path": os.path.join(BASE_DIRS, "temlpates"),
-    # "debug": True,
     "autoreload": True,
+    # "debug": True,
     # 设置tornado是否工作在调试模式下，默认为false即工作在生产下；
     # True：自动重启，监控源码是否有变动，判断是否重启服务器，减少手动重启，提高开发效率；autoreload = True
     # True：取消缓存编译的模版
